e4e4f975ecdb1876e8d536eeeeec6396b2fa0343
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-22 23:49:52 -0300

    creating product and category model

--- project/backend/categories/app/models.py
+++ project/backend/categories/app/models.py
@@ -1,1 +1,7 @@
 from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField)
+
+
+
+class Category(Model):
+    name = CharField(max_length=256)
+    created = DateTimeField(auto_now_add=True)
--- project/backend/products/app/models.py
+++ project/backend/products/app/models.py
@@ -1,1 +1,16 @@
-from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField)
+from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField, CASCADE)
+from backend.categories import Category
+
+
+
+
+class Product(Model):
+    name = CharField(max_length=256)
+    price = DecimalField(decimal_places=2, max_digits=20)
+    quantity = PositiveIntegerField()
+    category = ForeignKey(Category, on_delete=CASCADE)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name.upper()
