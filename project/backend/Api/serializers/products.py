260c3f26e6ff4e8aaf568503adac90bfb6007337
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-23 16:35:53 -0300

    correct representation for category fk

--- project/backend/Api/serializers/products.py
+++ project/backend/Api/serializers/products.py
@@ -1,3 +1,4 @@
+from backend.Api.serializers.categories import CategorySerializer
 from rest_framework import serializers
 from backend.products import Product
 
@@ -5,6 +6,11 @@ from backend.products import Product
 
 class ProductSerializer(serializers.ModelSerializer):
 
+    def to_representation(self, instance):
+        representation = super(ProductSerializer, self).to_representation(instance)
+        representation['category'] = instance.category.name
+        return representation
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = 'id', 'name', 'price', 'quantity', 'category', 'created', 'updated'
