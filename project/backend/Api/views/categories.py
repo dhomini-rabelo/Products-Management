347c7866b39706c334ef3d3448c2d642181c8a6c
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-24 16:37:03 -0300

    add cache on views

--- project/backend/Api/views/categories.py
+++ project/backend/Api/views/categories.py
@@ -3,12 +3,17 @@ from backend.categories import Category
 from ..actions.objects.categories import category_filters
 from Fast.django.api.views.filter import FilterView
 from rest_framework import generics
-
+from Fast.django.decorators.cache.api import control_cache_page, global_cache_page
+from django.utils.decorators import method_decorator
 
 
 class CategoryCreateAndListView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+    @method_decorator(global_cache_page(10))
+    def get(self, request, pk):
+        return super().get(request, pk)
 
 
 class FilterCategoryView(FilterView):
@@ -16,7 +21,15 @@ class FilterCategoryView(FilterView):
     filter_obj = category_filters
     model = Category
 
+    @method_decorator(global_cache_page(10))
+    def get(self, request, pk):
+        return super().get(request, pk)
+
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+    @method_decorator(control_cache_page(30))
+    def get(self, request, pk):
+        return super().get(request, pk)
--- project/backend/Api/views/products.py
+++ project/backend/Api/views/products.py
@@ -1,9 +1,10 @@
+from Fast.django.decorators.cache.api import control_cache_page, global_cache_page
 from backend.Api.serializers.products import ProductSerializer
 from backend.products import Product
 from Fast.django.api.views.filter import FilterView
 from ..actions.objects.products import product_filters
 from rest_framework import generics
-
+from django.utils.decorators import method_decorator
 
 
 
@@ -12,13 +13,26 @@ class ProductCreateAndListView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
+    @method_decorator(global_cache_page(10))
+    def get(self, request, pk):
+        return super().get(request, pk)
+
 
 class FilterProductView(FilterView):
     serializer_class = ProductSerializer
     filter_obj = product_filters
     model = Product
 
+    @method_decorator(global_cache_page(10))
+    def get(self, request, pk):
+        return super().get(request, pk)
+
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    @method_decorator(control_cache_page(30))
+    def get(self, request, pk):
+        return super().get(request, pk)
+        
